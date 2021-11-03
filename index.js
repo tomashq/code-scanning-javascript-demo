545064029535e397ed22537504f21c4041a2cb26
kurt <tomashq@gmail.com>
2021-11-03 17:19:31 +0100

    fixed security issue

--- index.js
+++ index.js
@@ -262,7 +262,9 @@ exports.extract = function (cwd, opts) {
       if (win32) return next() // skip links on win for now before it can be tested
       xfs.unlink(name, function () {
         var srcpath = path.resolve(cwd, header.linkname)
-
+        if(fs.existsSync(srcpath)) {
+          throw new Error('File already exists');
+        }
         xfs.link(srcpath, name, function (err) {
           if (err && err.code === 'EPERM' && opts.hardlinkAsFilesFallback) {
             stream = xfs.createReadStream(srcpath)
